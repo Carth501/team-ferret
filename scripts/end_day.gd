0389d454b7376108ccb583c98c4f93a1d6ac2125
Caleb Kopitzke <calebdk09@gmail.com>
2024-01-21 21:07:50 -0600

    Switches & buttons

---
+++ scripts/end_day.gd
@@ -0,0 +1,42 @@
+
+extends Control
+
+@onready var completed_day = $MarginContainer/VBoxContainer/CompletedDay as Label
+@onready var expenses_text = $MarginContainer/VBoxContainer/HBoxContainer/MiddleContainer/HBoxContainer/ExpensesText as RichTextLabel
+@onready var expenses_nums = $MarginContainer/VBoxContainer/HBoxContainer/MiddleContainer/HBoxContainer/ExpensesNums as RichTextLabel
+@onready var current_money = $MarginContainer/VBoxContainer/HBoxContainer/MiddleContainer/CurrentMoney as RichTextLabel
+@onready var stats = $MarginContainer/VBoxContainer/HBoxContainer/LeftContainer/Stats as RichTextLabel
+
+@export var startingMoney = 50
+@export var savings = 0
+@export var salary = 26
+var overtime = 0
+var sharesBonus = 0
+var money = 0
+@export var day = 0
+@export var rent = -15
+@export var food = -5
+
+
+func _ready():
+	update_expenses()
+	update_day_text()
+	
+func update_expenses() -> void:
+	if(day==0):
+		money = startingMoney
+	# Below will be code to update each variable based on previous day performance.
+	savings = money
+	#salary =
+	#overtime =
+	#sharesBonus =
+	#rent =
+	#food =
+	money = savings + salary + overtime + sharesBonus + rent + food
+
+func update_day_text() -> void:
+	day += 1
+	completed_day.text = str("DAY ", day)
+	expenses_nums.text = str("[right]\n", savings, "\n", salary, "\n", overtime,"\n[color=red]", rent,"\n", food,"[/color]\n", overtime)
+	current_money.text = str("[right]",money)
+
--- scripts/modules/switch_module.gd
+++ scripts/modules/switch_module.gd
@@ -1,18 +1,31 @@
-class_name switch_module extends abstract_module
+class_name switch_module
+extends abstract_module
 
 @export var button_obj: Button
 @export var label_obj: RichTextLabel
+@onready var switch_tex = $switch_tex
 
 func set_label(label: String):
 	label_obj.clear()
 	label_obj.append_text(label)
 
 func button_pressed():
-	var payload = {"id"= id, "value"= button_obj.button_pressed}
+	var state = button_obj.button_pressed
+	var payload = {"id"= id, "value"=state}
 	trigger.emit(payload)
+	toggle_switch(state)
 
 func get_current_values():
 	return {"value"= button_obj.button_pressed}
 
 func set_value(setting: bool):
 	button_obj.button_pressed = setting
+	toggle_switch(setting)
+
+func toggle_switch(state: bool) -> void:
+	var switch_on = "res://assets/images/modules/switch_on.png"
+	var switch_off = "res://assets/images/modules/switch_off.png"
+	if(state):
+		switch_tex.texture = load(switch_on)
+	else:
+		switch_tex.texture = load(switch_off)
