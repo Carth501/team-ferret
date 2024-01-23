20abaedbfc8cb7cb65184b9f49ff08b33c19ca79
Caleb Kopitzke <calebdk09@gmail.com>
2024-01-22 23:28:03 -0600

    ck-manual-book
    Removed templates from JSON files (no longer needed)
    Implemented book graphic for manual
    Recoded how the manual works so that it can use 2 pages
    Added structure to make it easier to call back to specific pages for later use.
    ~Bashed head into desk multiple times~

---
+++ scripts/manual_book.gd
@@ -0,0 +1,40 @@
+class_name manual_book_scene
+extends Node2D
+
+@onready var left_header = $TextureRect/MarginContainer/HBoxContainer/left_page/left_header as Label
+@onready var left_text = $TextureRect/MarginContainer/HBoxContainer/left_page/left_text as RichTextLabel
+@onready var right_header = $TextureRect/MarginContainer/HBoxContainer/right_page/right_header as Label
+@onready var right_text = $TextureRect/MarginContainer/HBoxContainer/right_page/right_text as RichTextLabel
+
+signal next_page
+signal prev_page
+
+var drag_offset: Vector2
+var initial_position: Vector2
+var mouse_over: bool = false
+var holding: bool = false
+
+func _on_gui_input(event):
+	if(event is InputEventMouseButton):
+		if(event.is_pressed() && mouse_over):
+			drag_offset = get_viewport().get_mouse_position()
+			initial_position = position
+			holding = true
+		else:
+			holding = false
+	elif(event is InputEventMouseMotion):
+		if(holding):
+			var delta_position = get_viewport().get_mouse_position() - drag_offset
+			position = initial_position + delta_position 
+
+func _on_mouse_entered():
+	mouse_over = true
+
+func _on_mouse_exited():
+	mouse_over = false
+
+func prev_button():
+	emit_signal("prev_page")
+
+func next_button():
+	emit_signal("next_page")
