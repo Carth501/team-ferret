36ae689f737b0a6086815205af944f6010b7029a
Caleb Kopitzke <calebdk09@gmail.com>
2024-01-25 17:23:31 -0600

    Implemented fullscreen setting
    Fully implemented

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -4,39 +4,49 @@ class_name settings extends Control
 @onready var master_id := AudioServer.get_bus_index("Master")
 @onready var music_id := AudioServer.get_bus_index("Music")
 @onready var effects_id := AudioServer.get_bus_index("Effects")
+@onready var toggle_fullscreen = $"TabContainer/    Graphics    /MarginContainer/VBoxContainer/View Settings/toggle_fullscreen" as Button
+
 @export var master_volume: Slider
 @export var music_volume: Slider
 @export var effects_volume: Slider
-@export var tog_fullscreen: CheckBox
+
 
 var config_path := "user://settings.cfg"
 var sound := "Sound"
 var graphic := "Graphics"
+var fullscreen:bool
 
 
 func _ready():
 	var err = config.load(config_path)
 	if(err != OK):
+		fullscreen = false
 		save_config()
 	else:
 		master_volume.value = config.get_value(sound, "master_volume")
 		music_volume.value = config.get_value(sound, "music_volume")
 		effects_volume.value = config.get_value(sound, "effects_volume")
+		fullscreen = config.get_value(graphic,"fullscreen")
 		apply_volumes()
+		apply_graphics()
 
 func save_config():
 		config.set_value(sound, "master_volume", master_volume.value)
 		config.set_value(sound, "music_volume", music_volume.value)
 		config.set_value(sound, "effects_volume", effects_volume.value)
-		#config.set_value(graphic,"window_setting",)
+		config.set_value(graphic,"fullscreen", fullscreen)
 		config.save(config_path)
 		apply_volumes()
+		apply_graphics()
 
 func apply_volumes():
 	process_slider(master_volume, master_id)
 	process_slider(music_volume, music_id)
 	process_slider(effects_volume, effects_id)
-	
+
+func apply_graphics():
+	toggle_fullscreen.button_pressed = fullscreen
+
 func set_mute_bus(bus_id: int, setting: bool):
 	AudioServer.set_bus_mute(bus_id, setting)
 
@@ -51,6 +61,8 @@ func process_slider(slider: Slider, bus_id: int):
 func _on_toggle_fullscreen(toggled_on):
 	if(toggled_on):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		fullscreen = true
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		DisplayServer.window_set_size(Vector2i(1280,720))
+		fullscreen = false
