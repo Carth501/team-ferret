0668312c8431c0d31abe542f878197e55d492c92
Carth501 <carth501@gmail.com>
2024-02-27 17:43:41 -0500

    restore manual movement

--- scripts/manual_book.gd
+++ scripts/manual_book.gd
@@ -14,6 +14,11 @@ signal prev_page
 signal to_index
 signal to_page(number: int)
 signal close
+var drag_offset: Vector2
+var initial_position: Vector2
+var mouse_over: bool = false
+var holding: bool = false
+var in_window: bool = true
 
 func prev_button():
 	emit_signal("prev_page")
@@ -31,3 +36,37 @@ func parse_hyperlink(meta: Variant):
 
 func close_manual():
 	close.emit()
+
+func _on_gui_input(event):
+	if(event is InputEventMouseButton):
+		if(event.is_pressed() && mouse_over):
+			drag_offset = get_viewport().get_mouse_position()
+			initial_position = position
+			holding = true
+		else:
+			holding = false
+	elif(event is InputEventMouseMotion):
+		if(holding && in_window):
+			var delta_position = get_viewport().get_mouse_position() - drag_offset
+			var new_position = initial_position + delta_position
+			#position = initial_position + delta_position 
+			var viewport_size = get_viewport_rect().size
+			var margin = Vector2(book_margin, book_margin)
+			var max_x = viewport_size.x - margin.x
+			var max_y = viewport_size.y - margin.y
+			new_position.x = clamp(new_position.x, 0, max_x)
+			new_position.y = clamp(new_position.y, 0, max_y)
+			position = new_position
+
+
+func _notification(what):
+	if what == NOTIFICATION_WM_MOUSE_ENTER:
+		in_window = true
+	elif what == NOTIFICATION_WM_MOUSE_EXIT:
+		in_window = false
+
+func _on_mouse_entered():
+	mouse_over = true
+
+func _on_mouse_exited():
+	mouse_over = false
